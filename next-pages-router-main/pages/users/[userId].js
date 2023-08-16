211c8c81b8e4c77fdca1725cb2dda3f1c8e5cd17
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-16 13:19:56 +0330

    dynamic-route

---
+++ next-pages-router-main/pages/users/[userId].js
@@ -0,0 +1,10 @@
+
+const Users = () => {
+    return (
+        <div>
+            <p>un</p>
+        </div>
+    );
+};
+
+export default Users;
---
+++ next-pages-router-main/pages/users/index.js
@@ -0,0 +1,12 @@
+
+const Users = () => {
+    return (
+        <div>
+            <h1>usersList</h1>
+            <p>user1</p>
+            <p>users2</p>
+        </div>
+    );
+};
+
+export default Users;
