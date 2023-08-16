17d09f6eacb4308a9bbe6c53b7285e7fcf82a078
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-16 17:04:39 +0330

    push-router

--- next-pages-router-main/pages/index.js
+++ next-pages-router-main/pages/index.js
@@ -1,14 +1,29 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const clickHandler = () => {
+
+    router.push("/student");
+
+  }
+
   return (
     <div>
       home
       <ul>
         <li>
           <Link href="/courses">go to course page</Link>
+          <br />
+          <Link href="/users">users</Link>
         </li>
       </ul>
+
+      <button onClick={clickHandler}>Login</button>
+
     </div>
   );
 }
