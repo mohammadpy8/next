e67848ebb7aa864110ab629be0cc95e61c672f5b
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-16 12:42:50 +0330

    page-router

---
+++ next-pages-router-main/pages/about-use.js
@@ -0,0 +1,9 @@
+const AboutUse = () => {
+    return (
+        <div>
+            about-us
+        </div>
+    );
+};
+
+export default AboutUse;
---
+++ next-pages-router-main/pages/courses.js
@@ -0,0 +1,10 @@
+
+const courses = () => {
+    return (
+        <div>
+            courses
+        </div>
+    );
+};
+
+export default courses;
