dcb64f66b3c5f72db89c1f60460f88faa4a54930
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 11:44:48 +0330

    fix project

--- next-pages-router-main/pages/index.js
+++ next-pages-router-main/pages/index.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import SearchBar from "../components/module/SearchBar";
+import Categoreis from "../components/module/Categoreis";
+import AllButton from "../components/module/AllButton";
+import CardPage from "../components/template/CarsPage";
+import carsData from "../data/carsData";
 
 const Home = () => {
+
+    const homeCars = carsData.slice(0, 3);
+
     return (
-        <div>
-            home
-        </div>
+        <>
+            <SearchBar />
+            <Categoreis />
+            <AllButton />
+            <CardPage data={homeCars} />
+        </>
     );
 };
 
