ba7d11600e901b6762e4858aedb7801e77665b57
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 08:42:53 +0330

    Card

--- next-pages-router-main/components/template/CarsPage.js
+++ next-pages-router-main/components/template/CarsPage.js
@@ -1,10 +1,11 @@
 import styles from "./CarsPage.module.css";
+import Card from "../module/Card";
 
 const CarsPage = ({data}) => {
   return (
     <div className={styles.container}>
       {data.map((car) => (
-        <p key={car.id}>{car.name}</p>
+          <Card key={car.id} {...car} />
       ))}
     </div>
   );
