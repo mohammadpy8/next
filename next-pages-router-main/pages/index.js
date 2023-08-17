6bc230b9b7fb6b720b28a0abcd15a3f1b2af12fc
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 13:38:50 +0330

    db

--- next-pages-router-main/pages/index.js
+++ next-pages-router-main/pages/index.js
@@ -1,20 +1,9 @@
-import SearchBar from "../components/module/SearchBar";
-import Categoreis from "../components/module/Categoreis";
-import AllButton from "../components/module/AllButton";
-import CardPage from "../components/template/CarsPage";
-import carsData from "../data/carsData";
 
 const Home = () => {
-
-    const homeCars = carsData.slice(0, 3);
-
     return (
-        <>
-            <SearchBar />
-            <Categoreis />
-            <AllButton />
-            <CardPage data={homeCars} />
-        </>
+        <div>
+            home
+        </div>
     );
 };
 
