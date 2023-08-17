c1174d4c094dc155ddbcfdb75618c05abdac99a0
mohammadpy8 <mohammadpy8@gmail.com>
2023-08-17 11:17:48 +0330

    searchbar-filter

--- next-pages-router-main/components/module/SearchBar.js
+++ next-pages-router-main/components/module/SearchBar.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
+import { useRouter } from "next/router";
 
 const SearchBar = () => {
-    return (
-        <div className={styles.container}>
-            <div>
-                <input type="text" placeholder="enter min price"/>
-                <input type="text" placeholder="enter max price"/>
 
-            </div>
+    const [min, setMin] = useState("");
+    const [max, setMax] = useState("");
 
-            <button>Search</button>
-        </div>
-    );
+    const router = useRouter();
+
+    const searchHandler = () => {
+        if (min && max) {
+            router.push(`/filter/${min}/${max}`);
+        } else {
+            alert("error"); 
+        }
+    }
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <input type="text" placeholder="enter min price" value={min} onChange={(e) => setMin(e.target.value)}/>
+        <input type="text" placeholder="enter max price" value={max} onChange={(e) => setMax(e.target.value)}/>
+      </div>
+
+      <button onClick={searchHandler}>Search</button>
+    </div>
+  );
 };
 
 export default SearchBar;
