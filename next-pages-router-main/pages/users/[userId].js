9e98a8a16e7d137c2720e6e21024d6a9520ba45b
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-17 09:19:48 +0330

    end-getStaticPaths

--- next-pages-router-main/pages/users/[userId].js
+++ next-pages-router-main/pages/users/[userId].js
@@ -9,23 +9,23 @@ const UserDetail = ({ user }) => {
 
 export default UserDetail;
 
+///get number of page and paths
+
 export async function getStaticPaths() {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+  const paths = data.map((user) => ({
+    params: {
+      userId: user.id.toString(),
+    }
+  }));
   return {
-    paths: [
-      { params: { userId: "1" } },
-      { params: { userId: "2" } },
-      { params: { userId: "3" } },
-      { params: { userId: "4" } },
-      { params: { userId: "5" } },
-      { params: { userId: "6" } },
-      { params: { userId: "7" } },
-      { params: { userId: "8" } },
-      { params: { userId: "9" } },
-      { params: { userId: "10" } },
-    ],
+    paths,
     fallback: false,
   };
-}
+};
+
+////get information of click on this page
 
 export async function getStaticProps(context) {
   const { params } = context;
