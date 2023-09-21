286bd317d359d864d7d33d83db1d818f5e56f8e0
mohammadpy8 <mohammadpy8@gmail.com>
2023-09-21 20:02:48 +0330

    SWR

--- next-pages-router-main/pages/about.js
+++ next-pages-router-main/pages/about.js
@@ -1,17 +1,25 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const About = () => {
-  const [data, setData] = useState([]);
+//   const [data, setData] = useState([]);
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/albums")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, []);
+//   useEffect(() => {
+//     fetch("https://jsonplaceholder.typicode.com/albums")
+//       .then((res) => res.json())
+//       .then((data) => setData(data));
+//   }, []);
+
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/albums",
+    (url) => fetch(url).then((res) => res.json())
+    );
+    
+    console.log(data, error);
 
   return (
     <div>
-      {data.length ? (
+      {data ? (
         data.map((item) => <h3 key={item.id}>{item.title}</h3>)
       ) : (
         <h1></h1>
